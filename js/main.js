cc5a138e2916872dcd26157e7d8ec75d83b6f041
Cyries <cyriesmadrid@gmail.com>
2020-06-01 15:18:52 +0800

    Layout 1 Final

--- js/main.js
+++ js/main.js
@@ -15,33 +15,6 @@ function cls(){
 
 
 
-var slideIndex;
-slideIndex = 1;
-
-function currentSlide(n){
-    showSlide(slideIndex = n)
-}
-
-function showSlide(n){
-    var i;
-    var slides;
-    slides = document.getElementsByClassName("slides");
-    
-    if(n > slides.length){slideIndex = 1}
-    if(n < 1){slideIndex = slides.length}
-    
-    for(i = 0; i < slides.length; i++){
-        slides[i].style.display = "none";
-    }
-    
-    slides[slideIndex - 1].style.display = "block";
-}
-
-function plusSlides(n){
-    showSlide(slideIndex += n);
-}
-
-
 
 
 
@@ -54,39 +27,39 @@ function plusSlides(n){
 
 
 //thanks god I finaly understand the process flow <3 <3 <3
-//MUST DECLARE FIRST SLIDEINDEX
+
+//you need first to declare initial index position of array equals to 1
+var slideIndex = 1;
+//this function execute the showSlides function with parameter and the result is to display the selected image as currentSlide;
+function currentSlide(n){         
+    showSlides(slideIndex = n);
+}
+
+//this fucntion contains following codes to execute:
+function showSlides(n) {  
+    //declarations
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    //if condition
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    //Loop Repetition for array slides  display none;
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    //display the value of array index position;
+    slides[slideIndex -1].style.display = "block";
+}
+
+//this function add parameter value 1 to slideIndex and execute the showSlides function
+function plusSlides(n) {          
+    showSlides(slideIndex += n);
+}
 
 
-//
-//var slideIndex = 1;
-//
-//function currentSlide(n){         
-//    showSlides(slideIndex = n);
-//}
-//
-//function showSlides(n) {          
-//    var i;
-//    var slides = document.getElementsByClassName("slides");
-//    
-//    if (n > slides.length) {slideIndex = 1}
-//    if (n < 1) {slideIndex = slides.length}
-//    
-//    //Loop Repetition for array slides  display none;
-//    for (i = 0; i < slides.length; i++) {
-//      slides[i].style.display = "none";
-//    }
-//    
-//    slides[slideIndex -1].style.display = "block";
-//}
-//
-//function plusSlides(n) {          
-//    showSlides(slideIndex += n);
-//}
-//
-//
-//
-//
-//
+
+
+
 
 
 
